e195980f9f75cce093d26335a30fcb7c665b37f9
phanit1 <babithota5@msitprogram.net>
2020-04-14 15:58:08 +0530

    Added methods without logic

--- LRU.py
+++ LRU.py
@@ -2,3 +2,9 @@ class LRUCache:
     def __init__(self):
         self.cache = []
         self.capacity = 4
+    def put(self,item):
+        pass
+    def get(self):
+        pass
+    def get_cache(self):
+        pass
