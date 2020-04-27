6d263ca246ca2dfb4272b805c234049758c7125c
phanit1 <babithota5@msitprogram.net>
2020-04-27 20:30:12 +0530

    Done with countChar method

--- javascript/beam.js
+++ javascript/beam.js
@@ -11,3 +11,17 @@ function countBs(str)
     return letter_Count;
 }
 console.log("Count of Bs is ",countBs('Ballons should be blown'))
+
+function countChar(str, letter) 
+{
+    var letter_Count = 0;
+    for (var position = 0; position < str.length; position++) 
+    {
+        if (str.charAt(position) == letter) 
+        {
+        letter_Count += 1;
+        }
+    }
+    return letter_Count;
+}
+console.log("Count is ",countChar('Phaneendhar','n'))
